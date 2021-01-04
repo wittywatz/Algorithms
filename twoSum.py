101a3320e4080ff65d9b5ea851bc656e86e47c81
WittyWatzz <35387099+WittyWatzz@users.noreply.github.com>
2021-01-04 11:42:02 -0500

    Add files via upload
    The worst-case run time complexity is O(n)

---
+++ twoSum.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+def twoSum(arr,target):
+    """
+    Takes an array of integers and a target variable
+    
+    Returns a list of indexes which sum up to the target or [] if otherwise
+    """
+    store = {}
+    for index,value in enumerate(arr):
+        difference = target-value
+        if difference in store.keys():
+            return [store[difference], index]
+        else:
+            store[value] = index
+    return []
+
+nums = [2,7,11,15]
+target = 9
+result = twoSum(nums,target)
+print(result)
