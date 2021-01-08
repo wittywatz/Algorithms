adee123185241a88e1b4c6bc7fc83a4ab5afb8f0
WittyWatzz <35387099+WittyWatzz@users.noreply.github.com>
2021-01-08 00:27:35 -0500

    Add files via upload
    Returns an integer of the longest sub-string encountered without repeating the alphabet

---
+++ longestSubStringWithoutRepeatingCharacters.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 00:23:08 2021
+
+@author: witty
+"""
+
+def longestSubStringLength(st):
+    str_longest = ""
+    max_so_far = 0
+    if st == "":
+        return 0
+    
+    for i in st:
+        if i in str_longest:
+            max_so_far = max(max_so_far, len(str_longest))
+            str_longest = i
+        else:
+             str_longest += i
+    # Caters for final case after loop exits, 
+    #can also place this in the else block, though it'll require more run time
+    max_so_far = max(max_so_far, len(str_longest))
+    return max_so_far
+
+print(longestSubStringLength("backlashed"))
