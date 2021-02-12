3298c75fd012839cdf05d34a55637c67b9935f90
Watson <35387099+WittyWatzz@users.noreply.github.com>
2021-02-11 21:54:27 -0500

    Update firstRecurringElement.js

--- Algorithms JavaScript/firstRecurringElement.js
+++ Algorithms JavaScript/firstRecurringElement.js
@@ -1,1 +1,14 @@
 //Code goes here
+const firstRecurringElement = (arr) =>{
+  let seen = {};
+  for (let i=0;i<arr.length; i++){
+    if(seen[arr[i]]){
+      return arr[i];
+    }
+    seen[arr[i]]= true;
+  }
+  return;
+}
+
+const result = firstRecurringElement([1,2,3,4])
+console.log(result)
