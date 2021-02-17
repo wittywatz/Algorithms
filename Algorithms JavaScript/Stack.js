5b7e662497dd1ea42bd1ee7a76a04d974e02f329
Watson <wittywatz@gmail.com>
2021-02-17 09:05:05 -0500

    Create Stack.js

---
+++ Algorithms JavaScript/Stack.js
@@ -0,0 +1,61 @@
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  } 
+}
+
+class Stack{
+  constructor(){
+    this.head = null; //Top of stack
+    this.tail = this.head; //Bottom of stack
+    this.length = 0;
+  }
+  top(){
+    if (!this.top){
+      return null;
+    }
+    return this.head.value
+  }
+  pop(){
+    if (this.length===0){
+      return
+    }
+    if(this.head===this.tail){
+      this.tail = null
+      this.head = null
+    }else{
+      this.head = this.head.next
+    }
+    this.length--
+  }
+  push(value){
+    const newNode = new Node(value)
+    if(this.head === null){
+      this.head = newNode
+      this.tail = this.head
+    }else{
+      newNode.next = this.head
+      this.head = newNode
+    }
+    
+    this.length++
+  }
+  size(){
+    return this.length
+  }
+  isEmpty(){
+    return this.head===null
+  }
+  printStackItems(){
+    const array = [];
+    let currentnode = this.head;
+    while(currentnode !== null){
+      array.push(currentnode.value)
+      currentnode = currentnode.next
+    }
+    return array
+  }
+}
+
+const myStack = new Stack
