93598c0e7bc90a6e310f3857bfcdccc12f341363
Watson <wittywatz@gmail.com>
2021-02-16 21:27:39 -0500

    Update addBinary.py

--- addBinary.py
+++ addBinary.py
@@ -3,7 +3,6 @@ def addBinaryNum(a,b):
     Takes two binary numbers, sums them and retuens their sum
 
     """
-    
     return bin((int(a,2)+int(b,2))).replace("0b","")
     
 print(addBinaryNum("101","101"))
