21f3197d744ccd240d9e33a3719fba085550465e
WittyWatzz <35387099+WittyWatzz@users.noreply.github.com>
2021-04-09 13:41:00 -0400

    Javascript Solution

---
+++ Algorithms Python/buy_sell_stock_once.py
@@ -0,0 +1,13 @@
+def buy_and_sell_stock_once(prices):
+  minimum_price = prices[0]
+  max_profit = 0
+  for price in prices:
+    max_profit_sell_today = price - minimum_price
+    print('sell today',max_profit_sell_today)
+    max_profit = max( max_profit, max_profit_sell_today)
+    print(max_profit)
+    minimum_price = min( minimum_price, price)
+    print('minimum', minimum_price)
+  return max_profit
+
+print(buy_and_sell_stock_once([310,315,275,295,260,270,290,230,255,250]))
